c695d3ec0bb7baf5f94b1833e2782f4efbc58f42
sbslc2000 <sbslc2000@gmail.com>
2023-10-12 18:42:14 +0900

    docker-compose 환경변수 수정

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: 'mysql',
-  user: 'root',
-  password: '12345678',
-  database: 'myapp'
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || '1234',
+  database: process.env.MYSQL_DATABASE || 'todoapp',
+  port: process.env.MYSQL_PORT || '3306'
 });
 exports.pool = pool;
