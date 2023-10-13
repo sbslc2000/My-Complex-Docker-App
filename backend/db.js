bcfbcf6c34fc35ec2af898be271bf305abdcec03
sbslc2000 <sbslc2000@gmail.com>
2023-10-13 12:19:53 +0900

    환경변수 로깅

--- backend/db.js
+++ backend/db.js
@@ -7,4 +7,11 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_DATABASE || 'todoapp',
   port: process.env.MYSQL_PORT || '3306'
 });
+
+console.log("MYSQL_HOST: ", process.env.MYSQL_HOST);
+console.log("MYSQL_USER: ", process.env.MYSQL_USER);
+console.log("MYSQL_PASSWORD: ", process.env.MYSQL_PASSWORD);
+console.log("MYSQL_DATABASE: ", process.env.MYSQL_DATABASE);
+console.log("MYSQL_PORT: ", process.env.MYSQL_PORT);
+
 exports.pool = pool;
